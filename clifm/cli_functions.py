698767e686b573b420dc91316c083f48296e7590
Regeace <playmarket_pochta@mail.ru>
2025-08-03 00:56:32 +0300

    обавлены функции копирования, посчёта и удаления файлов

--- clifm/cli_functions.py
+++ clifm/cli_functions.py
@@ -17,3 +17,32 @@ def make_file(file_name):
     """Создаёт новый файл."""
     with open(file_name, 'w+', encoding='utf-8') as _:
         pass
+
+
+def copy_file(file_name):
+    """Создаёт копию файла с метаданными."""
+    if not os.path.exists(file_name):
+        print('Файл не существует')
+        return
+    if os.path.exists('copy_' + file_name):
+        copy2(file_name, 'another_copy_' + file_name)
+    else:
+        copy2(file_name, 'copy_' + file_name)
+
+
+def count_files(dir_name):
+    """Подсчитывает количество файлов в папке."""
+    counter = 0
+    for base_dir, current_dir, files in os.walk(dir_name):
+        counter += len(files)
+    return counter
+
+
+def delete_file_or_catalog(entry_name):
+    """Удаляет папку (с файлами) или файл."""
+    if os.path.isfile(entry_name):
+        os.remove(entry_name)
+    else:
+        rmtree(entry_name)
+
+
