ee07cbefe38ca20da86aeecfa512eab381a1ca5a
Regeace <playmarket_pochta@mail.ru>
2025-08-03 01:20:14 +0300

    Добавлен парсер в базовый файл cli_file_manager.py

---
+++ cli_file_manager.py
@@ -0,0 +1,29 @@
+import argparse
+import clifm
+
+
+magic_words = {
+    'copy': clifm.copy_file,
+    'count': clifm.count_files,
+    'delete': clifm.delete_file_or_catalog,
+    'find': clifm.find_files,
+    'show': clifm.show_help,
+    'make': clifm.make_file
+}
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='clifm',
+                                     description='Небольшой файловый менеджер для консоли',
+                                     epilog='Для расширенной помощи дополнительно введите: python cli_file_manager.py show help')
+    parser.add_argument('magic_phrase', type=str, choices=magic_words.keys(), help='Команда взаимодействия с файлами и каталогами')
+    parser.add_argument('name', type=str, help='Имя файла или каталога')
+    parser.add_argument('-a', '--additional_var', type=str, help='Регулярное выражение для поиска')
+
+    args = parser.parse_args()
+
+    magic_words[args.magic_phrase](args.name)
+
+
+if __name__ == '__main__':
+    main()
